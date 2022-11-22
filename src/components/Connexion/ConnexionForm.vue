3e79a9dc9ea0be726fcfa7cab9395b32ad230bc4
giracle <clement.girardin@divtec.ch>
2022-11-22 12:24:56 +0100

    ajout code ConnexionForm.vue

---
+++ src/components/Connexion/ConnexionForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-form @submit.prevent="submitForm">
+    <q-input
+      outlined
+      v-model="form.email"
+      label="E-mail"
+      class="q-my-md"
+      :rules="[val => validateEmail(val) || 'Email invalide']"
+      lazy-rules
+    />
+
+    <q-input
+      type="password"
+      outlined
+      v-model="form.password"
+      label="Mot de passe"
+      class="q-my-md"
+      :rules="[ val => val.length >= 4 || 'Minimum 4 caractère']"
+      lazy-rules
+    />
+
+    <q-btn
+      type="submit"
+      color="primary"
+      label="Se connecter"
+    />
+  </q-form>
+</template>
+
+<script>
+export default {
+  name: 'ConnexionForm',
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      alert('Formulaire envoyé !')
+    },
+    validateEmail (email) {
+      // Source : https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(String(email).toLowerCase())
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/PageConnexion.vue
+++ src/pages/PageConnexion.vue
@@ -1,11 +1,51 @@
 <template>
   <q-page padding>
-    <h1>Connexion</h1>
+    <q-card className="connexion">
+      <q-tabs
+        v-model="tab"
+        className="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="connexion" label="Se connecter"/>
+        <q-tab name="enregistrement" label="Créer un compte"/>
+      </q-tabs>
+
+      <q-separator/>
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="connexion">
+          <div className="text-h6">Se connecter</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="enregistrement">
+          <div className="text-h6">Créer un compte</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageConnexion'
+  name: 'PageConnexion',
+  data () {
+    return {
+      tab: 'connexion'
+    }
+  }
 }
 </script>
+
+<style scoped>
+.connexion {
+  max-width: 500px;
+  width: auto;
+  margin: 0 auto;
+}
+</style>
+----------
