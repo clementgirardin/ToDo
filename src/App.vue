d96605ceb578b0fe751b94a806f52305f7c1372b
giracle <clement.girardin@divtec.ch>
2022-11-23 08:00:23 +0100

    chargement données LocalStorage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <router-view />
+  <div id="q-app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { mapActions } from 'vuex'
 
-export default defineComponent({
-  name: 'App'
-})
+export default {
+  name: 'App',
+  methods: {
+    ...mapActions('auth', ['setUser'])
+  },
+  mounted () {
+    // Récupère les données du localStorage
+    const user = this.$q.localStorage.getItem('user')
+    const token = this.$q.localStorage.getItem('token')
+    // Si un utilisateur et un token exisent
+    if (user && token) {
+      // Construction de l'objet data
+      const data = {
+        user,
+        access_token: token
+      }
+      // Définit l'utilisateur en cours
+      this.setUser(data)
+    }
+  }
+}
 </script>
